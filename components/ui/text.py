3ce0167887310ff9c365953f29777cdf80f61f9f
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-05-23 13:15:33 +0100

    updated font with font family

--- components/ui/text.py
+++ components/ui/text.py
@@ -1,12 +1,23 @@
 import wx
 from .colors import COLORS
 
+fonts = {
+    "roman": wx.FONTFAMILY_ROMAN,
+    "script": wx.FONTFAMILY_SCRIPT,
+    "decorative": wx.FONTFAMILY_DECORATIVE,
+    "swiss": wx.FONTFAMILY_SWISS,
+    "modern": wx.FONTFAMILY_MODERN,
+    "teletype": wx.FONTFAMILY_TELETYPE,
+    "default": wx.FONTFAMILY_DEFAULT,
+}
+
 
 class Text(wx.StaticText):
-    def __init__(self, parent, label, size=wx.DefaultSize, style=wx.ALIGN_CENTER):
+    def __init__(self, parent, label, size=wx.DefaultSize, style=wx.ALIGN_CENTER, font_size=10, font_family="default"):
         super().__init__(parent, label=label, size=size, style=style)
         self.SetForegroundColour(COLORS.GRAY_100)
         # make font monospace
-        font = wx.Font(10, wx.FONTFAMILY_TELETYPE,
+
+        font = wx.Font(font_size, fonts[font_family],
                        wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         self.SetFont(font)
