24742bdcd288a70a56ba1a77af40571f7633ad9d
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-05-25 12:08:25 +0100

    new scanner stuff

--- scanner.py
+++ scanner.py
@@ -28,9 +28,33 @@ class Symbol:
         """Initialise symbol properties."""
         self.type = None
         self.id = None
+        self.name = ""
+
+    def __repr__(self) -> str:
+        return f"Symbol(type={self.type}, id={self.id}, name={self.name})"
+
+# enum of symbol types
 
 
-class Scanner:
+class SymbolList:
+    def __init__(self):
+        self.HEADING = "HEADING"
+        self.LOGIC = "LOGIC"
+        self.NUMBER = "NUMBER"
+        self.NAME = "NAME"
+        self.INPUT = "INPUT"
+        self.OUTPUT = "OUTPUT"
+        self.EQUAL = "EQUAL"
+        self.DOT = "DOT"
+        self.COMMA = "COMMA"
+        self.SEMICOLON = "SEMICOLON"
+        self.OPEN_BRACKET = "OPEN_BRACKET"
+        self.CLOSE_BRACKET = "CLOSE_BRACKET"
+        self.HASHTAG = "HASHTAG"
+        self.EOF = "EOF"
+
+
+class Scanner(SymbolList):
 
     """Read circuit definition file and translate the characters into symbols.
 
@@ -52,21 +76,25 @@ class Scanner:
 
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
+        SymbolList.__init__(self)
         try:
             self.file = open(path, "r")
         except FileNotFoundError:
             raise Exception("Input file not found")
         self.names = names
-        self.symbol_list = [self.HEADING, self.LOGIC, self.NUMBER, self.NAME,
-                            self.EQUAL, self.DOT, self.COMMA, self.SEMICOLON,
-                            self.OPEN_BRACKET, self.CLOSE_BRACKET, self.HASHTAG, self.EOF] = range(12)
+
         self.heading_list = ["[devices]", "[conns]", "[monit]"]
-        [self.DEVICES_ID, self.CONNS_ID, self.MONIT_ID] = self.names.lookup(self.heading_list)
-        self.logic_list = ["DTYPE", "NAND", "NOR", "XOR", "AND", "OR", "CLOCK", "SWITCH"]
-        [self.DTYPE_ID, self.NAND_ID, self.NOR_ID, self.XOR_ID, 
-        self.AND_ID, self.OR_ID, self.CLOCK_ID, self.SWITCH_ID] = self.names.lookup(self.logic_list)
-        
-        self.current_character = ""
+
+        # [self.DEVICES_ID, self.CONNS_ID,
+        #     self.MONIT_ID] = self.names.lookup(self.heading_list)
+
+        self.logic_list = ["DTYPE", "NAND", "NOR",
+                           "XOR", "AND", "OR", "CLOCK", "SWITCH"]
+
+        [self.DTYPE_ID, self.NAND_ID, self.NOR_ID, self.XOR_ID,
+         self.AND_ID, self.OR_ID, self.CLOCK_ID, self.SWITCH_ID] = self.names.lookup(self.logic_list)
+
+        self.current_character = " "
         self.current_position = 0
         self.current_line = 0
         self.error_count = 0
@@ -77,66 +105,83 @@ class Scanner:
         symbol = Symbol()
         self.skip_spaces()  # current character now not whitespace
 
-        if self.current_character.isalpha():  # Names
+        if self.current_character.isalpha() or self.current_character == "[":
             name_string = self.get_name()
             self.name_string = name_string[0]
+            symbol.name = self.name_string
             if self.name_string in self.heading_list:
                 symbol.type = self.HEADING
             elif self.name_string in self.logic_list:
                 symbol.type = self.LOGIC
             else:
+                if symbol.name[-1] == "]" or name_string[1] == "[":
+                    # raise error
+                    self.error(SyntaxError, "Heading not valid")
+
                 symbol.type = self.NAME
             [symbol.id] = self.names.lookup([self.name_string])
-        
-        elif self.current_character.isdigit():  #Numbers
+
+        elif self.current_character.isdigit():  # Numbers
             symbol.id = self.get_number()
             symbol.type = self.NUMBER
-            
-        elif self.current_character == "=":  #Definitions
+
+        elif self.current_character == "=":  # Definitions
             symbol.type = self.EQUAL
+            symbol.name = "="
             self.advance()
 
-        elif self.current_character == ".":  #Connections
+        elif self.current_character == ".":  # Connections
             symbol.type = self.DOT
+            symbol.name = "."
             self.advance()
 
-        elif self.current_character == ",":  #Punctuations
+        elif self.current_character == ",":  # Punctuations
             symbol.type = self.COMMA
+            symbol.name = ","
             self.advance()
 
-        elif self.current_character == ";":  #Punctuations
+        elif self.current_character == ";":  # Punctuations
             symbol.type = self.SEMICOLON
+            symbol.name = ";"
             self.advance()
 
-        elif self.current_character == "(":  #Inputs No.
+        elif self.current_character == "(":  # Inputs No.
             symbol.type = self.OPEN_BRACKET
+            symbol.name = "("
             self.advance()
 
-        elif self.current_character == ")":  #Inputs No.
+        elif self.current_character == ")":  # Inputs No.
             symbol.type = self.CLOSE_BRACKET
+            symbol.name = ")"
             self.advance()
 
-        elif self.current_character == "#":  #Comments
+        elif self.current_character == "#":  # Comments
             symbol.type = self.HASHTAG
+            symbol.name = "#"
             self.advance()
             while self.current_character != "\n":
                 self.advance()
 
-        elif self.current_character == "":   #End of file
+        elif self.current_character == "":  # End of file
+            symbol.name = "EOF"
             symbol.type = self.EOF
 
-        else:  #Not a valid character
+        else:  # Not a valid character
             self.error(SyntaxError, "Character not valid")
-            
+
         self.current_position += 1
         return symbol
 
     def get_name(self):
         """Return the name and the next character that is non-alphanumeric"""
+        # self.advance()
         name = ""
+        self.skip_spaces()
         while self.current_character.isalnum() or self.current_character == "[" or self.current_character == "]":
             name += self.current_character
             self.advance()
+            if name[-1] == "]":
+                break
         return [name, self.current_character]
 
     def get_number(self):
@@ -164,7 +209,7 @@ class Scanner:
         else:
             self.current_position += 1
         return self.current_character
-   
+
     def get_current_line(self):
         """Return the current line from the file"""
         line = ""
@@ -174,11 +219,11 @@ class Scanner:
             line = self.file.readline()
         self.file.seek(current_position)
         return line
-    
+
     def error(self, error_type, message):
         """Error handling method"""
         self.error_count += 1
         print(f"Error {self.error_count} - {error_type.__name__}: {message}")
         print(f"Line {self.current_line}: {self.get_current_line()}")
-        print(" " * (self.current_position + 6) + "^")  # Marker to indicate the error position
-        
+        # Marker to indicate the error position
+        print(" " * (self.current_position + 6) + "^")
--- test_scanner.py
+++ test_scanner.py
@@ -1,26 +1,31 @@
 import pytest
-from scanner import *
+from scanner import Scanner, Symbol
 from names import Names
+
 
 @pytest.fixture
 def test_names():
     names = Names()
     return names
 
+
 @pytest.fixture
 def test_symbol():
     symbol = Symbol()
     return symbol
 
+
 @pytest.fixture
 def test_path():
-    path = "/Users/frankzhai/Desktop/gf2/docs/tests_file/test_file.txt"
+    path = "tests/test_file.txt"
     return path
+
 
 def test_get_symbol(test_names, test_path):
     """Test that names, numbers, symbols and keywords are all
     initialised and stored in the right sections"""
     test_scan = Scanner(test_path, test_names)
+
     test_string = [
         "[devices]",
         "G1",
@@ -28,11 +33,7 @@ def test_get_symbol(test_names, test_path):
         "G9",
         "=",
         "AND",
-        
-  
     ]
 
     for symbol in test_string:
         assert symbol == test_scan.get_symbol()
-
-    
