f51d843d19598c307a338724a33454e1e351fd2b
Yunfei Zhai <yz703@cam.ac.uk>
2023-05-30 09:47:17 +0100

    parser

--- test_parser.py
+++ test_parser.py
@@ -2,3 +2,21 @@ import pytest
 from scanner import Scanner, Symbol, ErrorCodes
 from names import Names
 from parser import Parser, ErrorCodes
+
+
+@pytest.fixture
+def test_names():
+    names = Names()
+    return names
+
+
+@pytest.fixture
+def test_scanner():
+    scanner = Scanner()
+    return scanner
+
+
+@pytest.fixture
+def test_parser():
+    parser = Parser()
+    return parser
